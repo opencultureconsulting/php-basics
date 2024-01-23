9832946b35f00ce7073a5253b18eccb29991b772
Sebastian Meyer <sebastian@informathekar.de>
2024-01-23 17:56:19 +0100

    Add PHP_CodeSniffer tooling

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * Useful PHP Basics
- * Copyright (C) 2023 Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
+ * PHP Basics
+ * Copyright (C) 2024 Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -11,11 +11,11 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 declare(strict_types=1);
@@ -30,9 +30,5 @@ namespace PhpCsFixer;
  */
 return (new Config())
     ->setRiskyAllowed(true)
-    ->setRules([
-        '@PSR12' => true,
-    ])
-    ->setFinder(
-        (new Finder())->in(__DIR__)
-    );
+    ->setRules(['@PSR12' => true])
+    ->setFinder((new Finder())->in(__DIR__));
