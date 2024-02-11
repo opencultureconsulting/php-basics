fec263c64b99964e6746fadcd193bf05d0b39cc6
Sebastian Meyer <sebastian@informathekar.de>
2024-02-11 19:05:56 +0100

    Add Overloading traits

---
+++ src/Traits/OverloadingSetter.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * PHP Basics
+ *
+ * Copyright (C) 2024 Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace OCC\Basics\Traits;
+
+/**
+ * Overloads a class with writable magic properties.
+ *
+ * Internally it writes the protected `$_data` array whose keys are interpreted
+ * as property names.
+ *
+ * > Example: `Foo->bar = 42` will set `Foo->_data['bar']` to `42`.
+ *
+ * @author Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
+ * @package Basics\Traits
+ */
+trait OverloadingSetter
+{
+    /**
+     * Holds the magically writable data.
+     *
+     * @var mixed[]
+     *
+     * @internal
+     */
+    protected array $_data = [];
+
+    /**
+     * Write data to an inaccessible property.
+     *
+     * @param string $property The class property to set
+     * @param mixed $value The new value of the property
+     *
+     * @return void
+     *
+     * @internal
+     */
+    public function __set(string $property, mixed $value): void
+    {
+        $this->_data[$property] = $value;
+    }
+
+    /**
+     * Unset an inaccessible property.
+     *
+     * @param string $property The class property to unset
+     *
+     * @return void
+     *
+     * @internal
+     */
+    public function __unset(string $property): void
+    {
+        unset($this->_data[$property]);
+    }
+}
